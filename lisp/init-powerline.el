b60c26b0d95e33f9c298a538dfe5eb75bc01fd4e
Elkins <jelkins@mytna.com>
2016-05-24 19:40:57 -0500

    Convert to selenized colors and personalize format
    TODO: the inactive powerline segments should have a slightly lighter
    color.
    TODO: the evil-mode powerline colors should use selenized colors.
    TODO: integrate the powerline face colors into the selenzied theme.

--- lisp/init-powerline.el
+++ lisp/init-powerline.el
@@ -1,20 +1,48 @@
+;;; init-powerline.el --- my setup for powerline
+
+;;; Commentary:
+;; Use the [selenized] colors, adapted to the solarized color name construct
+;;
+;; [selenized]: https://github.com/janek-warchol/selenized
+
+;;; Code:
+
+(setq my-pl-selenized
+      '((:base03  . "#154053")
+        (:base02  . "#245970")
+        (:base01  . "#7c95a0")
+        (:base00  . "#a8bcc3")
+        (:base0   . "#a8bcc3")
+        (:base1   . "#c4d8df")
+        (:base2   . "#e4e4e4")
+        (:base3   . "#ffffd7")
+        (:yellow  . "#d8b033")
+        (:orange  . "#d75f00")
+        (:red     . "#fc5851")
+        (:magenta . "#f16dc5")
+        (:violet  . "#5f5faf")
+        (:blue    . "#4e97f5")
+        (:cyan    . "#41c7b9")
+        (:green   . "#78b93e")))
+
 (defface my-pl-segment1-active
-  '((t (:foreground "#000000" :background "#E1B61A")))
+  `((t (:foreground ,(assoc-default :base02 my-pl-selenized) :background ,(assoc-default :base0 my-pl-selenized))))
   "Powerline first segment active face.")
-(defface my-pl-segment1-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
-  "Powerline first segment inactive face.")
 (defface my-pl-segment2-active
-  '((t (:foreground "#F5E39F" :background "#8A7119")))
+  `((t (:foreground ,(assoc-default :base2 my-pl-selenized) :background ,(assoc-default :base01 my-pl-selenized))))
   "Powerline second segment active face.")
-(defface my-pl-segment2-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
-  "Powerline second segment inactive face.")
 (defface my-pl-segment3-active
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  `((t (:foreground ,(assoc-default :base1 my-pl-selenized) :background ,(assoc-default :base02 my-pl-selenized))))
   "Powerline third segment active face.")
+
+(defface my-pl-segment1-inactive
+  `((t (:foreground ,(assoc-default :base0 my-pl-selenized) :background ,(assoc-default :base02 my-pl-selenized))))
+  "Powerline first segment inactive face.")
+(defface my-pl-segment2-inactive
+  `((t (:foreground ,(assoc-default :base0 my-pl-selenized) :background ,(assoc-default :base02 my-pl-selenized))))
+  "Powerline second segment inactive face.")
 (defface my-pl-segment3-inactive
-  '((t (:foreground "#CEBFF3" :background "#3A2E58")))
+  `((t (:foreground ,(assoc-default :base0 my-pl-selenized) :background ,(assoc-default :base02 my-pl-selenized))))
   "Powerline third segment inactive face.")
 
 (defun air--powerline-default-theme ()
@@ -27,39 +55,48 @@
                           (seg1 (if active 'my-pl-segment1-active 'my-pl-segment1-inactive))
                           (seg2 (if active 'my-pl-segment2-active 'my-pl-segment2-inactive))
                           (seg3 (if active 'my-pl-segment3-active 'my-pl-segment3-inactive))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (let ((evil-face (powerline-evil-face)))
-                                       (if evil-mode
-                                           (powerline-raw (powerline-evil-tag) evil-face)
-                                         ))
-                                     (if evil-mode
+                          (separator-left (if active (intern (format "powerline-%s-%s"
+                                                                     (powerline-current-separator)
+                                                                     (car powerline-default-separator-dir)))
+                                            (lambda (seg _) (powerline-raw (char-to-string #xe0b1) seg))))
+                          (separator-right (if active (intern (format "powerline-%s-%s"
+                                                                      (powerline-current-separator)
+                                                                      (cdr powerline-default-separator-dir)))
+                                             (lambda (seg _) (powerline-raw (char-to-string #xe0b3) seg))))
+                          (lhs (list (if active
+                                         (let ((evil-face (powerline-evil-face)))
+                                           (if evil-mode
+                                               (powerline-raw (powerline-evil-tag) evil-face))))
+                                     (if (and active evil-mode)
                                          (funcall separator-left (powerline-evil-face) seg1))
+
                                      ;;(when powerline-display-buffer-size
                                      ;;  (powerline-buffer-size nil 'l))
                                      ;;(when powerline-display-mule-info
                                      ;;  (powerline-raw mode-line-mule-info nil 'l))
-                                     (powerline-buffer-id seg1 'l)
-                                     (powerline-raw "[%*]" seg1 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format seg1 'l))
-                                     (powerline-raw " " seg1)
-                                     (funcall separator-left seg1 seg2)
+
                                      (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object seg2 'l))
-                                     (powerline-major-mode seg2 'l)
-                                     (powerline-process seg2)
-                                     (powerline-minor-modes seg2 'l)
-                                     (powerline-narrow seg2 'l)
+                                       (powerline-raw erc-modified-channels-object seg1 'l))
+                                     (powerline-major-mode seg1 'l)
+                                     (powerline-process seg1)
+                                     (powerline-minor-modes seg1 'l)
+                                     (powerline-narrow seg1 'l)
+                                     (powerline-raw " " seg1)
+
+                                     (funcall separator-left seg1 seg2)
+
+                                     (powerline-buffer-id seg2 'l)
+                                     (powerline-raw "[%*]" seg2 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format seg2 'l))
                                      (powerline-raw " " seg2)
+
                                      (funcall separator-left seg2 seg3)
+
                                      (powerline-vc seg3 'r)
                                      (when (bound-and-true-p nyan-mode)
                                        (powerline-raw (list (nyan-create)) seg3 'l))))
+
                           (rhs (list (powerline-raw global-mode-string seg3 'r)
                                      (funcall separator-right seg3 seg2)
                                      (unless window-system
@@ -79,7 +116,7 @@
 (use-package powerline
   :ensure t
   :config
-  (setq powerline-default-separator (if (display-graphic-p) 'slant
+  (setq powerline-default-separator (if (display-graphic-p) 'arrow
                                       nil))
   (air--powerline-default-theme))
 
@@ -87,3 +124,4 @@
   :ensure t)
 
 (provide 'init-powerline)
+;;; init-powerline.el ends here
